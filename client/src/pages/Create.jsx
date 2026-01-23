9dc549e779c3c56df2a45fa0ae971a548934062b
coderkids76-cell <coderkids76@gmail.com>
2026-01-23 12:33:21 +0000

    update

---
+++ client/src/pages/Create.jsx
@@ -0,0 +1,6 @@
+// src/pages/Create.jsx
+import React from 'react';
+export default function Create() {
+  return <div style={{color:'white', textAlign:'center', marginTop:'50px'}}>Create Page</div>;
+  }
+  
