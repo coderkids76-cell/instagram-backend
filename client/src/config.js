a0368e8ea87de550a172a381847b361916b349f5
coderkids76-cell <coderkids76@gmail.com>
2026-02-07 08:43:41 +0000

    update

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,1 @@
-// client/src/config.js
-export const API_URL = import.meta.env.VITE_API_URL || "https://unable-casson-eslam-bennani.koyeb.app";
+export const API_URL = import.meta.env.VITE_API_URL;
