b6b762ef2bd9cc66d54f7196906f15911e456c97
coderkids76-cell <coderkids76@gmail.com>
2026-02-07 08:35:12 +0000

    update

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,2 @@
 // client/src/config.js
-
-// الطريقة الأفضل: يقرأ الرابط من إعدادات Vercel تلقائياً
-export const API_URL = import.meta.env.VITE_API_URL || "https://unable-casson-eslam-bennani.koyeb.app/api";
+export const API_URL = import.meta.env.VITE_API_URL || "https://unable-casson-eslam-bennani.koyeb.app";
